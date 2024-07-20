ae7923e8fe05534ed000352d7e8e2980a96a85ae
Shubhahm Ingole <ishubham158@gmail.com>
2024-07-21 01:35:49 +0530

    add: home page, route and redux provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
+import { store } from './store/index.js';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <React.StrictMode>
+        <ReduxProvider store={store}>
+            <BrowserRouter basename='/'>
+                <App />
+            </BrowserRouter>
+        </ReduxProvider>
+    </React.StrictMode>
+);
