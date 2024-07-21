314d9cda6d28ed2f3c786abf9f709aa6b606d34a
Shubham Ingole <90132010+i9shubham@users.noreply.github.com>
2024-07-22 02:37:57 +0530

    add: frontend urls to remove cors error

--- server/main.py
+++ server/main.py
@@ -65,6 +65,8 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "https://main--daytrack.netlify.app",
+    "https://daytrack.netlify.app"
 ]
 
 app.add_middleware(
